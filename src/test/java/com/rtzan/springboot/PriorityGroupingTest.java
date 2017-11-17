e210fc6445836d9c6a71a15a7a140c3489eba051
marius_cornescu <marius_cornescu@yahoo.com>
2017-11-17 22:21:13 +0200

    init project with gradle

---
+++ src/test/java/com/rtzan/springboot/PriorityGroupingTest.java
@@ -0,0 +1,32 @@
+/**
+ * Free
+ */
+package com.rtzan.springboot;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+/**
+ */
+public class PriorityGroupingTest {
+
+    //~ ----------------------------------------------------------------------------------------------------------------
+    //~ Instance fields 
+    //~ ----------------------------------------------------------------------------------------------------------------
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+
+    @Before
+    public void setUp() {
+    }
+
+    @Test
+    public void test() throws Exception {
+    }
+
+
+}
